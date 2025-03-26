faa1e8b352bd0633dc279111b8ae3aa861db2dd9
MLK17 <melchior.a2r@gmail.com>
2025-03-27 00:07:25 +0100

    footer

--- app/layouts/MainLayout.js
+++ app/layouts/MainLayout.js
@@ -1,12 +1,9 @@
-import Footer from '@/app/components/Footer';
-
 export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow">
         {children}
       </main>
-      <Footer />
     </div>
   );
 }
